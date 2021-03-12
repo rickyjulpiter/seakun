e7817fb65bd1409e0f0feac42fe28cbaf9821de3
rickyjulpiter <rickyjulpiter14@gmail.com>
2021-03-13 02:37:39 +0700

    Deploy to Firebase

--- src/pages/order/order.component.js
+++ src/pages/order/order.component.js
@@ -1,30 +1,8 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 import OrderList from "../../components/order-list/order-list.component";
 
 export default class OrderComponent extends Component {
-    constructor() {
-        super();
-        this.state = {
-            invoices: [],
-        };
-    }
-
-    getInvoices = () => {
-        axios
-            .get(`http://demo2687090.mockable.io/order`)
-            .then((res) => {
-                let invoices = res.data;
-                this.setState({ invoices });
-            })
-            .catch((err) => console.log(err));
-    };
-
-    componentDidMount() {
-        this.getInvoices();
-    }
-
     render() {
         return (
             <div className="main_content">
@@ -32,14 +10,7 @@ export default class OrderComponent extends Component {
                 <div className="info">
                     <h1 className="mt-1">Order List</h1>
 
-                    {/* check when the data already exists */}
-                    {Object.keys(this.state.invoices).length === 0 ? (
-                        <></>
-                    ) : (
-                        <>
-                            <OrderList />
-                        </>
-                    )}
+                    <OrderList />
                 </div>
             </div>
         );
